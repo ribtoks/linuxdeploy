f33dd0b3b203b899e7d8d7d3be90edc4475e20f2
Taras Kushnir <kushnirTV@gmail.com>
2017-05-24 23:52:48 +0200

    Changed license to MIT

--- src/appdeployer.go
+++ src/appdeployer.go
@@ -1,22 +1,15 @@
 /*
  * This file is a part of linuxdeploy - tool for
  * creating standalone applications for Linux
+ *
  * Copyright (C) 2017 Taras Kushnir <kushnirTV@gmail.com>
  *
- * linuxdeploy is distributed under the GNU General Public License, version 3.0
- *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
+ * it under the terms of the MIT License.
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 package main
